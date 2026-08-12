229cf8c5c6b1c9aff3eb28887698143af827f552
siyaaa23 <pavanisole@gmail.com>
2026-08-12 06:24:44 +0530

    Create hello_world.c

---
+++ task-01/hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    // This statement prints "Hello World" on the screen
+    printf("Hello World");
+
+    // Indicates successful execution of the program
+    return 0;
+}
